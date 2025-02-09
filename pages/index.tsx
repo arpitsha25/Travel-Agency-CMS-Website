ffb68d1de7a62a9551283da1b9dea79248787b02
Arpit Sharma <arpitsharma@SD-GGN-LAP-014.local>
2025-02-09 15:04:35 +0530

    faqs

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,8 @@ export default function Home() {
       <Packages/>
       </div>
       <Services/>
-      <Testimonials/>
       <NewsletterSection/>
+      <Testimonials/>
       <FAQSection/>
     </div>
   );
